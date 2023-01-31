8921e00911eeb00a398b5a472e09fcf7ce3468d6
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2023-01-31 20:43:55 +0700

    create User#show and User#create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,4 +4,17 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+    if @event.save
+      redirect_to @event
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def event_params
+    params.require(:event).permit(:date, :location)
+  end
 end
