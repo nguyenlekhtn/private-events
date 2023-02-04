8878e24e0e1e40298731a890572db35f8320cecf
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2023-02-04 11:29:00 +0700

    add Attendances routes

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,2 +1,10 @@
 class AttendancesController < ApplicationController
+  def new
+    @event = Event.find(params[:id])
+  end
+
+  def create
+    @event = Event.find(params[:event_id])
+    current_user.attended_events << @event
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   root "events#index"
   resources :events, only: [:index, :new, :create, :show]
   resources :users, only: [:show]
-  resources :attendees_events, only: [:new, :create]
+  resources :attendances, only: [:new, :create]
 end
