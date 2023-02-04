667cb4b280dac173b784e08e9c011a7782f6cb3d
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2023-02-04 11:24:39 +0700

    destroy AttendeesEvents controller

--- app/helpers/attendees_events_helper.rb
+++
@@ -1,2 +0,0 @@
-module AttendeesEventsHelper
-end
--- test/controllers/attendees_events_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class AttendeesEventsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
