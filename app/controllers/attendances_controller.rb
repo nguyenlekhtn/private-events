aaccd93154747c2d5261ba4516afaee22d19da35
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2023-02-10 06:48:05 +0700

    refactor Attendaces Ctrl methods

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -2,12 +2,13 @@ class AttendancesController < ApplicationController
   before_action :authenticate_user!
 
   def new
-    @attendance = current_user.attendances.new
+    @event = Event.find(params[:event_id])
   end
 
   def create
     @event = Event.find(params[:event_id])
-    @event.attedandees << current_user
+    @event.attendees << current_user
+    redirect_to @event
   end
 
   def attedance_params
