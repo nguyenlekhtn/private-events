8fca9025c7c734c49e1a5f58fa6e1d8f78ab606f
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2023-02-10 21:09:46 +0700

    add Event class methods for pass and upcoming ev

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,12 @@ class Event < ApplicationRecord
   has_many :attendees, through: :attendances
 
   validates :date, :location, :name, presence: true
+
+  def self.past_events
+    where("date < ?", Date.current)
+  end
+
+  def self.upcoming_events
+    where("date >= ?", Date.current)
+  end
 end
