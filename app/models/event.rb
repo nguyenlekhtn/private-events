b24add05c5330850ceb21dfc20df8765d4374d38
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2023-02-11 20:57:44 +0700

    add delete event button to Event#show view

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
-  has_many :attendances
+  has_many :attendances, dependent: :destroy
   has_many :attendees, through: :attendances
 
   validates :date, :location, :name, presence: true
